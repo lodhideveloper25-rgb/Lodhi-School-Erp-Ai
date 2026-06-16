df56b3058f8cce361a3312aca98e3418589212b7
odhideveloper25-rgb <lodhideveloper25@gmail.com>
2026-06-16 18:46:40 +0500

    fix: backend auth register admin object

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -77,6 +77,8 @@ const registerSchool = async (req, res) => {
         _id: user._id,
         name: user.name,
         email: user.email,
+        role: user.role,
+        schoolCode: user.schoolCode,
         token: generateToken(user._id)
       }
     });
